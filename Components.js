693ff68313ef62d5ec70d5c917724e38deeae836
yous6637 <66566264+yous6637@users.noreply.github.com>
2022-09-03 08:44:55 +0100

    Update Components.js

--- Components.js
+++ Components.js
@@ -2,7 +2,7 @@ let ChartOptions = [{option:"1m",value:"60",selected:false},
 {option:"15m",value:"300",selected:false},
 {option:"60m",value:"900",selected:false},     {option:"30m",value:"1800",selected:false},
 {option:"1h",value:"3600",selected:false},{option:"2h",value:"7200",selected:false},{option:"4h (default)",value:"14400",selected:true},{option:"1d",value:"86400",selected:false},]
-export ChartOptions;
+export default ChartOptions;
 
 
 export function Signal()=>{
