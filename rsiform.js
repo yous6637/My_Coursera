6f53f0f13c5469ffb258709a8c64ab296096ef58
yous6637 <66566264+yous6637@users.noreply.github.com>
2022-09-06 08:04:29 +0100

    Update rsiform.js

--- rsiform.js
+++ rsiform.js
@@ -1,5 +1,24 @@
 
 
+
+ const colorPicker = new iro.ColorPicker('#picker', {
+        borderColor: "#ffffff",
+        borderWidth: 1,
+        width: Math.round(document.documentElement.clientWidth / 2),
+    });
+    colorPicker.on('color:change', function (color) {
+        document.body.style.background = color.hexString;
+    });
+
+    Telegram.WebApp.ready();
+    Telegram.WebApp.MainButton.setText('Choose Color').show().onClick(function () {
+        const data = JSON.stringify({hex: colorPicker.color.hexString, rgb: colorPicker.color.rgb});
+        Telegram.WebApp.sendData(data);
+        Telegram.WebApp.close();
+    });
+
+
+
 const setIndicatorSignalType =(op)=>{
  let nop = ""
   op=='buy'? nop='sell':nop='buy'
