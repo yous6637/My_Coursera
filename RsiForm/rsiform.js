274adc9392ec800960eaeda40c4619aa000a8b73
yous6637 <66566264+yous6637@users.noreply.github.com>
2022-09-06 14:12:46 +0100

    Update rsiform.js

--- RsiForm/rsiform.js
+++ RsiForm/rsiform.js
@@ -1,17 +1,21 @@
 
 
 
- const colorPicker = new iro.ColorPicker('#picker', {borderColor: "#ffffff",borderWidth: 1,width: Math.round(document.documentElement.clientWidth / 2)});
+ const colorPicker = new iro.ColorPicker('#picker', {
+        borderColor: "#ffffff",
+        borderWidth: 1,
+        width: Math.round(document.documentElement.clientWidth / 2),
+    });
+    colorPicker.on('color:change', function (color) {
+        document.body.style.background = color.hexString;
+    });
 
- colorPicker.on('color:change', function (color) {document.body.style.background = color.hexString;});
-
- Telegram.WebApp.ready();
- Telegram.WebApp.MainButton.setText('Choose Color').show().onClick(function () {const data = JSON.stringify({hex: colorPicker.color.hexString
-   ,rgb :colorPicker.color.rgb});
- Telegram.WebApp.sendData(data);
- 
- Telegram.WebApp.close();});
-    
+    Telegram.WebApp.ready();
+    Telegram.WebApp.MainButton.setText('Choose Color').show().onClick(function () {
+        const data = JSON.stringify({hex: colorPicker.color.hexString, rgb: colorPicker.color.rgb});
+        Telegram.WebApp.sendData(data);
+        Telegram.WebApp.close();
+    });
  
 
 const setIndicatorSignalType =(op)=>{
