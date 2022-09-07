485dd40c5b98bf7fd4694d626bfd4aaf1eca21b9
yous6637 <66566264+yous6637@users.noreply.github.com>
2022-09-07 12:40:20 +0100

    Update rsiform.js

--- RsiForm/rsiform.js
+++ RsiForm/rsiform.js
@@ -19,7 +19,7 @@ const setIndicatorSignalType =()=>{
    $('#sell').toggle();
 };
 const setConditionType = (event)=>{
- console.log(event):
+ console.log(event);
     
 };
 // $("#source2").change(function () {$("#condition2").text($(this).text())})
