0e7003489089557cd239468533ad8ddd8426df11
yous6637 <66566264+yous6637@users.noreply.github.com>
2022-09-08 18:50:16 +0100

    Update rsiform.js

--- RsiForm/rsiform.js
+++ RsiForm/rsiform.js
@@ -1,30 +1,30 @@
 
- const TelApp = Telegram.WebApp ;
+//  const TelApp = Telegram.WebApp ;
 
- TelApp.ready();
+//  TelApp.ready();
  
-function setThemeClass() {document.documentElement.className = Telegram.WebApp.colorScheme;}
-Telegram.WebApp.onEvent('themeChanged', setThemeClass);            
- setThemeClass();        
+// function setThemeClass() {document.documentElement.className = Telegram.WebApp.colorScheme;}
+// Telegram.WebApp.onEvent('themeChanged', setThemeClass);            
+//  setThemeClass();        
         
        
 
 
- console.log(TelApp.colorScheme);
+//  console.log(TelApp.colorScheme);
 
   
-  $("body").addClass(TelApp.colorSheme);
+//   $("body").addClass(TelApp.colorSheme);
 
- TelApp.MainButton.setText('Choose Color').show().onClick(function () {
- const data = JSON.stringify({"chart periode": $("#ChartPeriode").val(),"s1" :$("#ohlc").val(),"periode" :$("#periode").val(),"condition" :$("#condition").val(),"s2" :$("#sourc2").val(),"val" :$("#value").val()});
- TelApp.sendData(data);
- TelApp.close();
-    });
+//  TelApp.MainButton.setText('Choose Color').show().onClick(function () {
+//  const data = JSON.stringify({"chart periode": $("#ChartPeriode").val(),"s1" :$("#ohlc").val(),"periode" :$("#periode").val(),"condition" :$("#condition").val(),"s2" :$("#sourc2").val(),"val" :$("#value").val()});
+//  TelApp.sendData(data);
+//  TelApp.close();
+//     });
 
-const setIndicatorSignalType =()=>{ $('#buy').toggle(); $('#sell').toggle();};
+// const setIndicatorSignalType =()=>{ $('#buy').toggle(); $('#sell').toggle();};
 
 
 $("#source2").change(function() { var str = "";$("#source2 option:selected").each(function() {str += $(this).text()=="Value"? "Value":"Periode" + " ";});
       $("#condition2").text(str);}).change();
 
-const showHideIndicatorDescription = ()=>{$("#editIndicatorDescription").toggle("slow");}
+// const showHideIndicatorDescription = ()=>{$("#editIndicatorDescription").toggle("slow");}
